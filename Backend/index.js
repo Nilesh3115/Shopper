3e1a4c5cd49a86b7dd7cc553adaf5681850e076b
Nilesh <RN9319672@GMAIL.COM>
2025-06-13 17:45:11 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -374,7 +374,9 @@ app.listen(port, (error) => {
     app.post("/upload", upload.single('product'), (req, res) => {
         res.json({
             success: 1,
-            image_url: `http://localhost:${port}/images/${req.file.filename}`,
+         //   image_url: `http://localhost:${port}/images/${req.file.filename}`,
+            // image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         });
     });
     
