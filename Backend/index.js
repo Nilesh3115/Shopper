3c6e7d43250e3d97868c683dfe8cbe59aa797b8d
Nilesh <RN9319672@GMAIL.COM>
2025-06-13 17:38:56 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -26,6 +26,7 @@ const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+        
     }
 
 })
@@ -38,7 +39,8 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        // image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
     });
 });
 
