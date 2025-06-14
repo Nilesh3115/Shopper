a802612b8303a4f040690c0e4046a4704b10127e
N-stack-a11y <raghuvanshinitin62@gmail.com>
2025-06-14 15:35:06 +0530

    Modified Index.js

--- Frontend/src/Components/Item/Item.jsx
+++ Frontend/src/Components/Item/Item.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/** import React from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom'
 
@@ -20,4 +20,39 @@ const Item = (props) => {
   )
 }
 
-export default Item
+export default Item */
+
+
+import React from 'react';
+import './Item.css';
+import { Link } from 'react-router-dom';
+
+const Item = (props) => {
+  console.log(props, "4545454");
+
+  // Backend URL
+  const backendURL = "https://shopper-b.onrender.com";
+
+  return (
+    <div className='item'>
+      <Link to={`/product/${props.id}`}>
+        <img 
+          onClick={() => window.scrollTo(0, 0)} 
+          src={`${backendURL}/images/${props.image}`} 
+          alt="" 
+        />
+      </Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          ${props.new_price}
+        </div>
+        <div className="item-price-old">
+          ${props.old_price}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
